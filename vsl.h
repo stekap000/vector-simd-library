b756659e07900d01f7615557586dbad02bb16558
stekap000 <stekap000@gmail.com>
2024-03-06 00:43:19 +0100

    V4i type and add,sub for it

--- vsl.h
+++ vsl.h
@@ -4,6 +4,7 @@
 // TODO: Write checks for availability of intrinsics.
 // For now, we just assume this is valid.
 #include <immintrin.h>
+#include <stdint.h>
 
 #define VSLAPI static
 
@@ -18,22 +19,31 @@ typedef union {
 	float arr[4];
 } vsl_V4f;
 
+typedef union {
+	struct {
+		uint32_t v0, v1, v2, v3;
+	};
+	struct {
+		uint32_t x, y, z, w;
+	};
+	__m128i vec;
+	uint32_t arr[4];
+} vsl_V4i;
+
+
 VSLAPI inline vsl_V4f vsl_v4f_add(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_sub(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_mul(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_div(vsl_V4f v, vsl_V4f w);
 
+VSLAPI inline vsl_V4i vsl_v4i_add(vsl_V4i v, vsl_V4i w);
+VSLAPI inline vsl_V4i vsl_v4i_sub(vsl_V4i v, vsl_V4i w);
+
 
 
 VSLAPI inline void vsl_v4f_print(vsl_V4f v);
 
-//__m128
-//__m128i
-//__m128d
- 
-//__m256i
-
-#endif
+#endif // VSL_H
 
 #ifdef VSL_IMPLEMENTATION
 
@@ -51,6 +61,14 @@ VSLAPI inline vsl_V4f vsl_v4f_mul(vsl_V4f v, vsl_V4f w) {
 
 VSLAPI inline vsl_V4f vsl_v4f_div(vsl_V4f v, vsl_V4f w) {
 	return (vsl_V4f)_mm_div_ps(v.vec, w.vec);
+}
+
+VSLAPI inline vsl_V4i vsl_v4i_add(vsl_V4i v, vsl_V4i w) {
+	return (vsl_V4i)_mm_add_epi32(v.vec, w.vec);
+}
+
+VSLAPI inline vsl_V4i vsl_v4i_sub(vsl_V4i v, vsl_V4i w) {
+	return (vsl_V4i)_mm_sub_epi32(v.vec, w.vec);
 }
 
 
