23636daa478945874871986169f874e2bd20b11d
stekap000 <stekap000@gmail.com>
2024-03-06 22:56:28 +0100

    Shuffle mask macro, sq, and partial work on cross product

--- vsl.h
+++ vsl.h
@@ -5,8 +5,13 @@
 // For now, we just assume this is valid.
 #include <immintrin.h>
 #include <stdint.h>
+#include <assert.h>
 
 #define VSLAPI static
+
+#define VSL_NOT_IMPLEMENTED(msg) assert(!(msg))
+
+#define VSL_MM_SHUFFLE_MASK(c3, c2, c1, c0) (((c3) << 6) + ((c2) << 4) + ((c1) << 2) + (c0))
 
 typedef union {
 	struct {
@@ -35,6 +40,9 @@ VSLAPI inline vsl_V4f vsl_v4f_add(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_sub(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_mul(vsl_V4f v, vsl_V4f w);
 VSLAPI inline vsl_V4f vsl_v4f_div(vsl_V4f v, vsl_V4f w);
+VSLAPI inline float vsl_v4f_dot(vsl_V4f v, vsl_V4f w);
+VSLAPI inline vsl_V4f vsl_v4f_cross(vsl_V4f v, vsl_V4f w);
+VSLAPI inline vsl_V4f vsl_v4f_sq(vsl_V4f v);
 
 VSLAPI inline vsl_V4i vsl_v4i_add(vsl_V4i v, vsl_V4i w);
 VSLAPI inline vsl_V4i vsl_v4i_sub(vsl_V4i v, vsl_V4i w);
@@ -61,6 +69,48 @@ VSLAPI inline vsl_V4f vsl_v4f_mul(vsl_V4f v, vsl_V4f w) {
 
 VSLAPI inline vsl_V4f vsl_v4f_div(vsl_V4f v, vsl_V4f w) {
 	return (vsl_V4f)_mm_div_ps(v.vec, w.vec);
+}
+
+VSLAPI inline float vsl_v4f_dot(vsl_V4f v, vsl_V4f w) {
+	VSL_NOT_IMPLEMENTED("Dot product not yet implemented");
+	return 0;
+}
+
+VSLAPI inline vsl_V4f vsl_v4f_cross(vsl_V4f v, vsl_V4f w) {
+	// V = (v0, v1, v2, 0)
+	// W = (w0, w1, w2, 0)
+	// 	
+	// V x W = (v1w2 - v2w1,
+	//  	    v2w0 - v0w2,
+	// 		    v0w1 - v1w0
+	//          0)
+	
+	// From intel intrinsics doc.
+	// Keeping this here for now because I always forget shuffle intrinsic mask positions.
+	//DEFINE SELECT4(src, control) {
+	//	CASE(control[1:0]) OF
+	//	0:	tmp[31:0] := src[31:0]
+	//	1:	tmp[31:0] := src[63:32]
+	//	2:	tmp[31:0] := src[95:64]
+	//	3:	tmp[31:0] := src[127:96]
+	//	ESAC
+	//	RETURN tmp[31:0]
+	//}
+	//dst[31:0] := SELECT4(a[127:0], imm8[1:0])
+	//dst[63:32] := SELECT4(a[127:0], imm8[3:2])
+	//dst[95:64] := SELECT4(b[127:0], imm8[5:4])
+	//dst[127:96] := SELECT4(b[127:0], imm8[7:6])
+	
+	__m128 tmp0 = _mm_shuffle_ps(v.vec, v.vec, VSL_MM_SHUFFLE_MASK(3, 0, 2, 1));
+	__m128 tmp1 = _mm_shuffle_ps(w.vec, w.vec, VSL_MM_SHUFFLE_MASK(3, 1, 0, 2));
+	__m128 tmp2 = _mm_shuffle_ps(v.vec, v.vec, VSL_MM_SHUFFLE_MASK(3, 1, 0, 2));
+	__m128 tmp3 = _mm_shuffle_ps(w.vec, w.vec, VSL_MM_SHUFFLE_MASK(3, 0, 2, 1));
+	
+	return (vsl_V4f)_mm_sub_ps(_mm_mul_ps(tmp0, tmp1), _mm_mul_ps(tmp2, tmp3));
+}
+
+VSLAPI inline vsl_V4f vsl_v4f_sq(vsl_V4f v) {
+	return vsl_v4f_mul(v, v);
 }
 
 VSLAPI inline vsl_V4i vsl_v4i_add(vsl_V4i v, vsl_V4i w) {
